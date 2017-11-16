9228f6492e4cf631bafccfb9469d8e8a2de9c626
chen shalev <chensha07@gmail.com>
2017-11-16 13:22:18 +0200

    second

--- actorCtrl.js
+++ actorCtrl.js
@@ -8,17 +8,16 @@ app.controller("actorCtrl", function($scope) {
         this.img = img;
         this.link =link;
     }  
-  
-    $scope.actors = [
-        new Actor("nikol", 
+     $scope.actors = [
+        new Actor("Nikol", 
         "kidman",
         "https://images-na.ssl-images-amazon.com/images/M/MV5BMTk1MjM5NDg4MF5BMl5BanBnXkFtZTcwNDg1OTQ4Nw@@._V1_UY317_CR10,0,214,317_AL_.jpg",
         "http://www.imdb.com/name/nm0000173/"),
-        new Actor("julia", 
+        new Actor("Julia", 
         "Roberts",
         "https://images-na.ssl-images-amazon.com/images/M/MV5BMTQzNjU3MDczN15BMl5BanBnXkFtZTYwNzY2Njc4._V1_UX214_CR0,0,214,317_AL_.jpg",
         "http://www.imdb.com/name/nm0000210/"),
-        new Actor("meg", 
+        new Actor("Meg", 
         "Ryan",
         "https://images-na.ssl-images-amazon.com/images/M/MV5BMTgzOTI0NzI3OV5BMl5BanBnXkFtZTgwODIyMzUzMDI@._V1_UY317_CR1,0,214,317_AL_.jpg",
         "http://www.imdb.com/name/nm0000212/?ref_=tt_cl_t1"),
@@ -26,10 +25,10 @@ app.controller("actorCtrl", function($scope) {
         "Aniston",
         "https://images-na.ssl-images-amazon.com/images/M/MV5BNjk1MjIxNjUxNF5BMl5BanBnXkFtZTcwODk2NzM4Mg@@._V1_UY317_CR3,0,214,317_AL_.jpg",
         "http://www.imdb.com/name/nm0000098/"),
-        new Actor("", 
-        "kidman",
-        "https://images-na.ssl-images-amazon.com/images/M/MV5BMTk1MjM5NDg4MF5BMl5BanBnXkFtZTcwNDg1OTQ4Nw@@._V1_UY317_CR10,0,214,317_AL_.jpg",
-        "http://www.imdb.com/name/nm0000173/mediaviewer/rm328971520?ref_=nm_ov_ph"),
+        new Actor("Jennifer", 
+        "Lopez",
+        "https://images-na.ssl-images-amazon.com/images/M/MV5BMTY0OTY3ODA3OV5BMl5BanBnXkFtZTcwMzMyMzQ1NQ@@._V1_UY317_CR32,0,214,317_AL_.jpg",
+        "http://www.imdb.com/name/nm0000182/"),
         new Actor("Jennifer", 
         "Garner",
         "https://images-na.ssl-images-amazon.com/images/M/MV5BNTA4MDgzMTc1Nl5BMl5BanBnXkFtZTcwNjQyMjIwNQ@@._V1_UY317_CR14,0,214,317_AL_.jpg",
@@ -37,5 +36,9 @@ app.controller("actorCtrl", function($scope) {
     
     ];
 
-
+    $scope.order =  function() {
+        $scope.orderProp="first";
+    }
+    
+  
     });
